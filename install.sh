d1d078c80a91312a7bfc066455c5df11c656c4ef
Guntars Simanskis <guntars@diamondgraphics.ie>
2014-11-01 16:57:21 +0000

    Simplifying install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,4 @@
 #!/bin/sh
-DIRECTORY="../domtemplate";
-if [ ! -d "$DIRECTORY" ]; then
-    echo "please clone DOMTemplate from https://bitbucket.org/gunsim/domtemplate"
-else
-    cd ${DIRECTORY}
     npm install
-    grunt
-    cd "../widget"
-    npm install
-    npm install ${DIRECTORY}/domtemplate*
     bower install
     grunt
-fi
