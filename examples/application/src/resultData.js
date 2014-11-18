d00320fd378db9335f283c3d1f35ce1e2614369b
Guntars Simanskis <guntars@diamondgraphics.ie>
2014-11-18 17:44:44 +0000

    Added shrink data in passing data

--- examples/application/src/resultData.js
+++ examples/application/src/resultData.js
@@ -5,58 +5,54 @@ define([
     'chartData'
 ], function (chartData) {
     return {
-        tabledata: {
-            table: {
-                thead: {
-                    headvalue: '#',
-                    headset: 'Set',
-                    headelement: 'Element',
-                    headtest: 'test',
-                    headother: 'Other'
+        table: {
+            thead: {
+                headvalue: '#',
+                headset: 'Set',
+                headelement: 'Element',
+                headtest: 'test',
+                headother: 'Other'
+            },
+            tbody: [
+                {
+                    value: {
+                        text: '1,001',
+                        color: 'red'
+                    },
+                    set: 'Lorem',
+                    element: 'ipsum',
+                    test: 'dolor',
+                    other: 'sit',
+                    class: 'warning'
                 },
-                tbody: [
-                    {
-                        value: {
-                            text: '1,001',
-                            color: 'red'
-                        },
-                        set: 'Lorem',
-                        element: 'ipsum',
-                        test: 'dolor',
-                        other: 'sit',
-                        class: 'warning'
+                {
+                    value: {
+                        text: '1,002',
+                        color: 'blue'
                     },
-                    {
-                        value: {
-                            text: '1,002',
-                            color: 'blue'
-                        },
-                        set: 'amet',
-                        element: 'consectetur',
-                        test: 'adipiscing',
-                        other: 'elit',
-                        class: 'danger'
+                    set: 'amet',
+                    element: 'consectetur',
+                    test: 'adipiscing',
+                    other: 'elit',
+                    class: 'danger'
 
+                },
+                {
+                    value: {
+                        text: '1,003',
+                        color: 'yellow'
                     },
-                    {
-                        value: {
-                            text: '1,003',
-                            color: 'yellow'
-                        },
-                        set: 'Integer',
-                        element: 'nec',
-                        test: 'odio',
-                        other: 'Praesent',
-                        class: 'info'
-                    }
-                ]
-            }
-        },
-        chartdata: {
-            chart: {
-                content: {
-                    data: chartData
+                    set: 'Integer',
+                    element: 'nec',
+                    test: 'odio',
+                    other: 'Praesent',
+                    class: 'info'
                 }
+            ]
+        },
+        chart: {
+            content: {
+                data: chartData
             }
         }
     }
