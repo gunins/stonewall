0a9b9a72310e067a12cd96ee539b2c619882cf88
Guntars Simanskis <guntars@diamondgraphics.ie>
2015-03-19 16:18:13 +0000

    Release v0.2.19

--- dist/dev/widget/Constructor.js
+++ dist/dev/widget/Constructor.js
@@ -1536,10 +1536,11 @@ define('widget/parsers/setRoutes',[
 
                         if (args.length > 0) {
                             dataSet.link = args;
-                            if (instance && instance.to) {
-                                instance.to.apply(instance, args.concat(params));
+                        }
 
-                            }
+                        if (instance && instance.to) {
+                            instance.to.apply(instance, args.concat(params));
+
                         }
 
                     });
