c8260500454e4a82db708ec3f1f5a9a614ecdd32
Guntars Simanskis <guntars@diamondgraphics.ie>
2015-05-05 16:28:56 +0100

    Fixed dom.add function

--- src/widget/dom.js
+++ src/widget/dom.js
@@ -97,7 +97,12 @@ define([
             //console.log(fragment, parent, data);
             el.placeholder = fragment.querySelector('#' + el._node.id) ||
                              createPlaceholder(el._node.data.tag || el.el.tagName);
+
             el.el = el.run.call(el, fragment, false, parent, data);
+
+            if (el._node && el._node.data && el._node.data.instance) {
+                utils.extend(el, el._node.data.instance);
+            }
 
         },
         // Adding text in to node
